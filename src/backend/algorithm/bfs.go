41e483ab6c37b11e516951e89ac081e26ceb0099
zaki <zakiyudhistira.itb69@gmail.com>
2024-04-22 11:42:41 +0700

    feat : bfs (unfinished)

--- src/backend/algorithm/bfs.go
+++ src/backend/algorithm/bfs.go
@@ -1,7 +1,33 @@
 package algorithm
 
-// import (
-// 	"scraper/models"
-// )
+import ( 
+	// "fmt"
+	"scraper/api"
+	"scraper/models"
+)
 
-// TO DO
+func RunBFS(pageUrl string, target string) []models.Page{
+	solution := make([]models.Page, 0, 5)
+	visited := make(map[string]bool)
+	visited[pageUrl] = true
+	runBFSHelper(pageUrl, target, &solution, visited)
+	return solution
+}
+
+func runBFSHelper(pageUrl string, target string, solution *[]models.Page, visited map[string]bool) {
+    results := api.Scraper(pageUrl)
+    // found := false
+
+    for i := 0; i < len(results); i++ {
+		if(!visited[results[i].Url]){
+			visited[results[i].Url] = true
+			if results[i].Url == target {
+				*solution = append(*solution, results[i])
+				// found = true
+				return
+			}
+		}
+    }
+
+	
+}
