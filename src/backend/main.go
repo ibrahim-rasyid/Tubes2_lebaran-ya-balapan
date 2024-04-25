a861f7355a68395beeb91a86fbad0a1d88ea54d2
zaki <zakiyudhistira.itb69@gmail.com>
2024-04-25 10:04:34 +0700

    debug

--- src/backend/main.go
+++ src/backend/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"scraper/algorithm"
+	// "scraper/api"
 	"fmt"
 )
 
 func main(){
-	fmt.Println(algorithm.RunBFS("https://en.wikipedia.org/wiki/Joko_Widodo", "https://en.wikipedia.org/wiki/War"))
+	fmt.Println(algorithm.RunBFS("https://en.wikipedia.org/wiki/Joko_Widodo", "https://en.wikipedia.org/wiki/Volodymyr_Zelenskyy"))
 }
