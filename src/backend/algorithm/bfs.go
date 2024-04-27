661f46038fffe00c5a9d4fc90feed7de2803d48b
zaki <zakiyudhistira.itb69@gmail.com>
2024-04-27 18:41:25 +0700

    fix : BFS concurrency

--- src/backend/algorithm/bfs.go
+++ src/backend/algorithm/bfs.go
@@ -1,26 +1,25 @@
 package algorithm
+// Breadth first search implementation
 
 import (
-	"fmt"
 	"scraper/models"
+	"fmt"
 	"sync"
 	"time"
 )
 
-// type Queue []models.Page
+/*
+=== MUST READ BEFORE USING ===
+The function takes 5 main arguments
+if all result are needed to be returned, input max_result as 0 and use_max_res as false
+example :
 
-// func enqueue(q *Queue, element models.Page){
-// 	*q = append((*q), element)
-// }
+anything := algorithm.RunBFS("https://en.wikipedia.org/wiki/Adolf_Hitler", "https://en.wikipedia.org/wiki/United_Kingdom", 20, 0, false)
 
-// func dequeue(q *Queue) models.Page {
-// 	element := (*q)[0]
-// 	*q = (*q)[1:]
-// 	return element
-// }
+*/
 
-func RunBFS(pageUrl string, target string) [][]models.Page{
-	start := time.Now()
+func RunBFS(pageUrl string, target string, max_goroutine int, max_result int, use_max_res bool) models.Response{	// Run BFS function
+
 	visited := make(map[string]bool)
 	step := make(map[models.Page]models.Page)
 	visited[pageUrl] = true
@@ -33,27 +32,30 @@ func RunBFS(pageUrl string, target string) [][]models.Page{
 	var input = []models.Page{main_page}
 
 	// ret, depth, count := runBFSHelper(pageUrl, target, visited, step, input, 1, 0)
-	ret, depth, count := runBFSGoRoutine(pageUrl, target, visited, input, step, 0, 10)
+	ret, depth, count, runtime := runBFSGoRoutine(pageUrl, target, visited, input, step, 0, max_goroutine, max_result, use_max_res) // Main BFS function
 	fmt.Println(depth , " ", "count : ", count)
-	// ret = append(ret, main_page)
 
-	for i, j := 0, len(ret)-1; i < j; i, j = i+1, j-1 {
+	for i, j := 0, len(ret)-1; i < j; i, j = i+1, j-1 { // Result reversal for valid output
 		ret[i], ret[j] = ret[j], ret[i]
 	}
 
-	// for i := range ret {
-	// 	if ret[i].Title == ""{
-	// 		ret[i].Title = api.GetTitle(ret[i].Url)
-	// 	}
-	// }
+	for i := range ret { // appending result with start page
+		ret[i] = append(ret[i], main_page)
+		ret[i] = reverse(ret[i])
+	}
 
-	fmt.Println(time.Since((start)))
+	var return_val models.Response
 
-	return ret
+	return_val.N_step = depth
+	return_val.Accessed = count
+	return_val.Time = runtime
+	return_val.Steps = ret
+
+	return return_val
 }
 
 func runBFSHelper(start string, target string, visited map[string]bool, step map[models.Page]models.Page, to_be_processed []models.Page, depth int, count int) ([]models.Page, int, int){
-	
+	// run BFS without concurrentcy
 	var new_process []models.Page
 	
 	for i := range to_be_processed {
@@ -70,19 +72,6 @@ func runBFSHelper(start string, target string, visited map[string]bool, step map
 				step[temp[j]] = to_be_processed[i]
 
 				if temp[j].Url == target {
-					// var ret []models.Page
-					// step_temp := temp[j]
-
-					// ret = append(ret, step_temp)
-
-					// for {
-					// 	if step[step_temp].Url == start {
-					// 		break
-					// 	} else {
-					// 		ret = append(ret, step[step_temp])
-					// 		step_temp = step[step_temp]
-					// 	}
-					// }
 					return getPath(start, step, temp[j]), depth, len(visited)
 				}
 			} else {
@@ -96,71 +85,90 @@ func runBFSHelper(start string, target string, visited map[string]bool, step map
 	return runBFSHelper(start, target, visited, step, new_process, depth, count)
 }
 
-func runBFSGoRoutine(start string, target string, visited map[string]bool, to_be_processed []models.Page, step map[models.Page]models.Page, depth int, max_go int) ([][]models.Page, int, int){
+func runBFSGoRoutine(start string, target string, visited map[string]bool, to_be_processed []models.Page, step map[models.Page]models.Page, depth int, max_go int, max_res int, ismax bool) ([][]models.Page, int, int, float64){
 	st := time.Now()
+	
 	guard := make(chan struct{}, max_go)
 	var lock sync.Mutex
 	var wg sync.WaitGroup
+	// Go routine guard, wait group, and mutex definition
 
-	var next_process []models.Page
-
-	var result [][]models.Page
-
-	// added := make(map[string]bool)
+	var next_process []models.Page // Used as queue
+	var result [][]models.Page // Path result slice
+	added := make(map[string]bool) // Validator for added url
+	// Required data initialization
 
 	for i := range to_be_processed {
 		guard <- struct{}{}
 		wg.Add(1)
+		// Go routine constraints prep
 
-		go func (page models.Page) {
-			defer func(){
-				<-guard
-				wg.Done()
-				// fmt.Println("Terminate " + page.Title)
-			}()
-			temp := api.Scraper(page.Url)
-			lock.Lock()
+			go func (page models.Page) {
+				defer func(){
+					<-guard
+					wg.Done()
+					// Routine temrination
+				}()
+				
+				temp := Scraper(page.Url) // Scraping
 
-			visited[page.Url] = true
+				lock.Lock() // Lock mutex due to writing data
 
-			for j := range temp {
-				if !visited[temp[j].Url]{
-					_, val := step[temp[j]]
-					if !val{
-						step[temp[j]] = page
-					}
-					next_process = append(next_process, temp[j])
-					if temp[j].Url == target {
-						fmt.Println("found from " + page.Url)
-						var path_i []models.Page
-						if page.Url == start {
+				visited[page.Url] = true
 
-						} else {
-							path_i = getPath(start, step, page)
+				for j := range temp {
+					if !visited[temp[j].Url]{
+						_, val := step[temp[j]]
+						if !val{
+							step[temp[j]] = page
 						}
-						// path_i = append(path_i, temp[j])
-						result = append(result, path_i)
-						break
+						if !added[temp[j].Url]{
+							next_process = append(next_process, temp[j])
+							added[temp[j].Url] = true
+						}
+						if temp[j].Url == target { // Found result process
+							fmt.Println("found from " + page.Url)
+							var path_i []models.Page
+							if page.Url == start {
+	
+							} else {
+								path_i = getPath(start, step, page)
+							}
+							path_i = reverse(path_i)
+							path_i = append(path_i, temp[j])
+							path_i = reverse(path_i)
+							// Adding path to result slice of path
+							
+							result = append(result, path_i)
+							break // Break out of loop since a result is found
+						}
 					}
 				}
-			}
-			lock.Unlock()
-		}(to_be_processed[i])
+				lock.Unlock() // Unlock mutex
+
+			}(to_be_processed[i])
+
+		if(len(result) >= max_res && ismax){ // if a maximum amount on answer is not omitted
+			break
+		}
 	}
 
-	wg.Wait()
-	fmt.Println(len(result))
-	fmt.Println(time.Since(st))
+	wg.Wait() // Wait for go routines termination
+	// fmt.Println(len(result)) debugging purpose
+	// fmt.Println(time.Since(st))
 
-	if len(result) == 0{
-		depth++
-		return runBFSGoRoutine(start, target, visited, next_process, step, depth, max_go)
+	total_time := time.Since(st).Seconds()
+
+	if len(result) == 0 {
+		depth++ // depth increment
+		return runBFSGoRoutine(start, target, visited, next_process, step, depth, max_go, max_res, ismax)
 	} else {
-		return result, depth, len(visited)
+		return result, depth, len(visited), total_time
 	}
 }
 
 func reverse(ret []models.Page) []models.Page {
+	// Slice reversal
 	for i, j := 0, len(ret)-1; i < j; i, j = i+1, j-1 {
 		ret[i], ret[j] = ret[j], ret[i]
 	}
@@ -168,6 +176,7 @@ func reverse(ret []models.Page) []models.Page {
 }
 
 func getPath(start string, step map[models.Page]models.Page, page models.Page) []models.Page {
+	// Path treading algorithm
 	var ret []models.Page
 	step_temp := page
 
