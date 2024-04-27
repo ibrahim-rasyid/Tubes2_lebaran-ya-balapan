886c0306ea60c3e6f1d79d756b4e135b47345363
Ibrahim Rasyid <thamrinihsan@gmail.com>
2024-04-27 17:06:13 +0700

    Feat: Working on concurrency

--- src/backend/algorithm/ids.go
+++ src/backend/algorithm/ids.go
@@ -3,6 +3,7 @@ package algorithm
 import (
 	// "fmt"
 	"scraper/models"
+	"sync"
 	"time"
 )
 
@@ -52,6 +53,68 @@ func DLS(currentPage models.Page, targetPage models.Page, visited map[string]boo
 			visited[page.Url] = true
 			steps = append(steps, page)
 			next := DLS(page, targetPage, visited, steps, depth-1)
+			if next != nil {
+				return append([]models.Page{currentPage}, next...)
+			}
+			steps = steps[:len(steps)-1]
+		}
+	}
+	return nil
+}
+
+func IDSGoroutine(startTitle string, targetTitle string, startUrl string, targetUrl string) models.Result {
+	var result models.Result
+	var steps []models.Page
+
+	var startPage models.Page
+	startPage.Title = startTitle
+	startPage.Url = startUrl
+
+	var targetPage models.Page
+	targetPage.Title = targetTitle
+	targetPage.Url = targetUrl
+	depth := 0
+
+	start := time.Now()
+	var wg sync.WaitGroup
+	for {
+		visited := make(map[string]bool)
+		// fmt.Println(startPage)
+		// fmt.Println(targetPage)
+		if depth != 0 {
+			wg.Add(1)
+		}
+		path := DLSGoroutine(startPage, targetPage, visited, steps, depth, &wg)
+		wg.Wait()
+		if path != nil {
+			end := time.Now()
+			exe_time := end.Sub(start)
+			result.Accessed = len(visited)
+			result.Steps = path
+			result.N_step = depth
+			result.Time = exe_time.Seconds()
+			return result
+		}
+		depth++
+	}
+}
+
+func DLSGoroutine(currentPage models.Page, targetPage models.Page, visited map[string]bool, steps []models.Page, depth int, wg *sync.WaitGroup) []models.Page {
+	defer wg.Done()
+	if currentPage.Url == targetPage.Url {
+		return []models.Page{currentPage}
+	}
+	if depth <= 0 {
+		return nil
+	}
+
+	childUrl := Scraper(currentPage.Url)
+	for _, page := range childUrl {
+		wg.Add(1)
+		if !visited[page.Url] {
+			visited[page.Url] = true
+			steps = append(steps, page)
+			next := DLSGoroutine(page, targetPage, visited, steps, depth-1, wg)
 			if next != nil {
 				return append([]models.Page{currentPage}, next...)
 			}
--- src/backend/main.go
+++ src/backend/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	// "fmt"
-	// "scraper/algorithm"
-	"scraper/api"
+	"fmt"
+	"scraper/algorithm"
+	// "scraper/api"
 )
 
 func main() {
-	// result := algorithm.IDS("Prabowo Subianto", "Jakarta", "https://en.wikipedia.org/wiki/Prabowo_Subianto", "https://en.wikipedia.org/wiki/Jakarta")
-	// fmt.Printf("%d\n%d\n", result.Accessed, result.N_step)
-	// for i, step := range result.Steps {
-	// 	if i != 0 {
-	// 		fmt.Print(" --> ")
-	// 	}
-	// 	fmt.Printf("%s", step.Title)
-	// }
-	// fmt.Printf("\n%f\n", result.Time)
-	api.Init();
+	result := algorithm.IDSGoroutine("Prabowo Subianto", "Jakarta", "https://en.wikipedia.org/wiki/Prabowo_Subianto", "https://en.wikipedia.org/wiki/Jakarta")
+	fmt.Printf("%d\n%d\n", result.Accessed, result.N_step)
+	for i, step := range result.Steps {
+		if i != 0 {
+			fmt.Print(" --> ")
+		}
+		fmt.Printf("%s", step.Title)
+	}
+	fmt.Printf("\n%f\n", result.Time)
+	// api.Init();
 }
